ebbc057fc6c852ae9fb9699c3ecdb786b5d8fc95
sivakrishna <sivadurusala@gmail.com>
2025-02-07 16:48:18 -0500

    Added list_operations.py script

---
+++ list_operations.py
@@ -0,0 +1,14 @@
+>>> list_a = [0, 1, 2, 3, 4, 5, 6]
+>>> print(list_a[4])
+4
+>>> print(list_a[-3:])
+[4, 5, 6]
+>>> print(len(list_a))
+7
+>>> dict_a = {'a': 1, 'b': 2}
+>>> print(dict_a.keys() )
+dict_keys(['a', 'b'])
+>>> print(dict_a.values() )
+dict_values([1, 2])
+>>> print(dict_a['b'])
+2
